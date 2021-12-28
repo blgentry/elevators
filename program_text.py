976b4520f788c0addebec6bdda78c873ec065f76
Brian Gentry <bgentry@M1-Mac-Mini.local>
2021-12-28 17:35:54 -0500

    program_text class:  initial checkin with a few values

---
+++ program_text.py
@@ -0,0 +1,9 @@
+
+class program_text:
+    def __init__(self):
+        self.title = "ELEVATOR SIMULATOR by Brian L. Gentry"
+        self.main_menu = "Main Menu"
+        self.select_car = "Elevator:"
+        self.setup_elevators = "Number of Elevators:"
+        self.setup_floors = "Number of Floors:"
+        self.quit = f"Thank you for using {self.title}"
