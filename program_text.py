a8e0dca0b42cc090699d013b07006121b93cbac3
Brian Gentry <bgentry@M1-Mac-Mini.local>
2022-01-08 14:37:36 -0500

    changed "self" to "s"

--- program_text.py
+++ program_text.py
@@ -1,11 +1,11 @@
 
 class program_text:
-    def __init__(self):
-        self.title = "ELEVATOR SIMULATOR by Brian L. Gentry"
-        self.main_menu = "Main Menu"
-        self.select_car = "Elevator:"
-        self.select_floor = "Floor:"
-        self.select_direction = "Direction:"
-        self.setup_elevators = "Number of Elevators:"
-        self.setup_floors = "Number of Floors:"
-        self.quit = f"Thank you for using {self.title}"
+    def __init__(s):
+        s.title = "ELEVATOR SIMULATOR by Brian L. Gentry"
+        s.main_menu = "Main Menu"
+        s.select_car = "Elevator:"
+        s.select_floor = "Floor:"
+        s.select_direction = "Direction:"
+        s.setup_elevators = "Number of Elevators:"
+        s.setup_floors = "Number of Floors:"
+        s.quit = f"Thank you for using {self.title}"
